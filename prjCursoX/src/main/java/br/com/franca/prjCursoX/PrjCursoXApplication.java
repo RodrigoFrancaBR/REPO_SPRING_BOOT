11f96ca6105334c9dcb1676a791003b9b57dabb9
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2018-03-14 18:19:45 -0300

    criação do projeto Hello World!

---
+++ prjCursoX/src/main/java/br/com/franca/prjCursoX/PrjCursoXApplication.java
@@ -0,0 +1,12 @@
+package br.com.franca.prjCursoX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjCursoXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjCursoXApplication.class, args);
+	}
+}
